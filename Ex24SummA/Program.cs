4d69a037f1ac1ea70e4c2b8c0b4bb318cb680c76
Oleksii Ishchenko <optionalzirelleariz>
2023-08-20 00:52:36 -0600

    initial commit

---
+++ Ex24SummA/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReturnNumber(string two_digit)
+{
+    int i;
+    int result = 1;
+
+    int FirstDigit = int.Parse(two_digit[0].ToString());
+    int SecondDigit = int.Parse(two_digit[1].ToString());
+
+    for(i=0; i < SecondDigit; i++)
+    {
+        result = result * FirstDigit;
+    }
+    
+    return result;
+}
+
+Console.WriteLine("Your Input: ");
+string UserInput = Console.ReadLine();
+int power = ReturnNumber(UserInput);
+Console.WriteLine(power);
+
+
+
+
+
+// int FillNumber(string message)
+// {
+//     Console.WriteLine($"Input: {message}");
+//     int number = int.Parse(Console.ReadLine());
+//     return number;
+// }
---
+++ ex27SummOfDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int AllSumm(string array)
+{
+    int result = 0; 
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        result += int.Parse(array[i].ToString()); 
+    }
+
+    return result;
+}
+
+Console.WriteLine("Your Input: ");
+string UserInput = Console.ReadLine();
+int power = AllSumm(UserInput);
+Console.WriteLine(power);
